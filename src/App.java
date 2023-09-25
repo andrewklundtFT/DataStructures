19f04c403fbf536c427e29b0a7c94c5126821b7a
andrewklundtFT <ak1036178@student.francistuttle.edu>
2023-09-25 15:35:20 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        boolean izGettinRunnted = true;
+        Scanner scanner = new Scanner(System.in);
+        HashMap<String, String> student = new HashMap(String, String);
+        while (izGettinRunnted) {
+            System.out.println("1 - Add student\n2 - Find student\n3 - Delete student\n4 - Show all Students\n5 - Exit");
+            
+        }
+
+    }
+}
